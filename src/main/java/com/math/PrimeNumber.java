a6591234440e25a505e9d456cd566d3cb4485d1c
Ramesh Kumar Koyya <rameshkumarkoyya@outlook.com>
2022-07-18 22:57:02 +0530

    Prime Number Check Efficient Approach

--- src/main/java/com/math/PrimeNumber.java
+++ src/main/java/com/math/PrimeNumber.java
@@ -5,11 +5,10 @@ public class PrimeNumber {
 	/*
 	 *  17 is prime or not 
 	 *  start with 2, 4, 8, 16, 32 > 17 so 17 is prime 
-	 * 
 	 */
 
 	public static void main(String[] args) {
-		System.out.println(isPrime(65));
+		System.out.println(isPrime(1031));
 	}
 	
 	private static boolean isPrime(int numb) {
@@ -17,8 +16,18 @@ public class PrimeNumber {
 			return false;
 		}
 		
-		for(int i = 2; i * i <= numb; i++) {
-			if(numb % i == 0) {
+		if(numb == 2 || numb == 3) {
+			return true;
+		}
+		
+		if(numb % 2 == 0 || numb % 3 == 0) {
+			return false;
+		}
+		
+		for(int i = 5; i * i <= numb; i = i + 6) {
+			int secondNumber = i + 2;
+			System.out.println("Numbers are : "+ i + " Second Number " + secondNumber);
+			if(numb % i == 0 || numb % (i + 2) == 0) {
 				return false;
 			}
 		}
